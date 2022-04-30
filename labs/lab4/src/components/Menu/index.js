5a0eb171371f9ae1860dedfdb85b55db3c1a44d5
simonfevrier <simonfevrier123@gmail.com>
2022-04-29 22:45:56 -0400

    April 29, 2022 10:45 PM

--- labs/lab4/src/components/Menu/index.js
+++ labs/lab4/src/components/Menu/index.js
@@ -1,10 +1,5 @@
 import MenuItem from '../MenuItem';
-<ImageList
-    sx={{ width: 500, height: 450 }}
-    variant="quilted"
-    cols={4}
-    rowHeight={121}
->
+
 export default function Menu({ menuName, menuItems }) {
     return (
         <div>
@@ -12,16 +7,7 @@ export default function Menu({ menuName, menuItems }) {
             {
                 menuItems.map((item, index) =>
                     <MenuItem key={index} {...item} ></MenuItem>)
-                < ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
-            <img
-                {...srcset(item.img, 121, item.rows, item.cols)}
-                alt={item.title}
-                loading="lazy"
-            />
-        </ImageListItem>
-    ))
-}
-</ImageList >
+                    
             }
         </div>
     )
