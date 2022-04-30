70a76604a22b6418c215576a2262a2e37530f935
simonfevrier <simonfevrier123@gmail.com>
2022-04-29 22:57:55 -0400

    April 29, 2022 10:57 PM

--- labs/lab4/src/components/MenuItem/index.js
+++ labs/lab4/src/components/MenuItem/index.js
@@ -1,11 +1,25 @@
 export default function MenuItem({ itemId, itemPrice, itemName, ItemDescription }) {
     return (
+        <View style={styles.container}>
+            <Text style={repStyles.reps}>{count}</Text>
+            <Button
+                buttonStyle={{
+                    backgroundColor: 'black',
+                    borderWidth: 2,
+                    borderColor: 'white',
+                    borderRadius: 30,
+                }}
+                containerStyle={{
+                    width: 200,
+                    marginHorizontal: 50,
+                    marginVertical: 10,
+                }}
         <div className="menu-item" id={`menu-item-${itemId}`}>
             <span>${itemPrice}</span>
             <h2>{itemName}</h2>
             <p>{ItemDescription}</p>
             <button>Add to Cart</button>
         </div>
-    
+    </View>
     );
 }
